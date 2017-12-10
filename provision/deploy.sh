c5dd246ef2ccc3f4129e025182bff8f108e7db2a
gosho-kazuya <ketsume0211@gmail.com>
2017-12-10 15:27:03 +0900

    add provision

---
+++ provision/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+aws s3 sync --delete dist/ s3://nhknews/ --profile kazuya
+aws configure set preview.cloudfront true
+aws cloudfront create-invalidation --distribution-id E3O29DRB7JJ6T --paths '/*'
