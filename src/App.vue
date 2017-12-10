060dd7c8349c2095d35e606368e345be0f8ae6f7
gosho-kazuya <ketsume0211@gmail.com>
2017-12-10 16:15:40 +0900

    add loading and header

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,46 @@
 <template>
   <div id="app">
-    <div class="article" v-for="article in articles">
-      <div class="article-title">
-        {{ article.title }}
-      </div>
-      <div class="article-content">
-        {{ article.content }}
+    <div class="header">
+      aaa
+    </div>
+
+    <div class="spinner" v-if="loading">
+      <mt-spinner color="#33bd2a" type="snake"></mt-spinner>
+    </div>
+
+    <div class="article-container" v-else>
+      <div class="article" v-for="article in articles">
+        <div class="article-title">
+          {{ article.title }}
+        </div>
+        <div class="article-content">
+          {{ article.content }}
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import axios from 'axios'
+import { Spinner } from 'mint-ui';
+
+Vue.component(Spinner.name, Spinner);
 
 export default {
   name: 'app',
   data () {
     return {
-      articles: []
+      articles: [],
+      loading: true
+
     }
   },
   mounted () {
     axios.get('/public/data.json').then(res => {
       this.articles = res.data
+      this.loading = false
     })
   }
 }
@@ -36,8 +53,11 @@ body {
   -moz-osx-font-smoothing: grayscale;
   color: #666;
   font-size: 12px;
-  background: #f7f7f7;
+}
+
+#app {
   line-height: 1.5;
+  background: #f7f7f7;
   box-sizing: border-box;
 }
 
@@ -45,8 +65,33 @@ h1, h2 {
   font-weight: normal;
 }
 
+.header {
+  height: 48px;
+  background: #33bd2a;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  color: #fff;
+  font-size: 18px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 1px 4px rgba(0,0,0,.15);
+}
+
+.spinner {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+}
+
+.article-container {
+  padding-top: 60px;
+}
+
 .article {
-  margin: 8px 8px 12px;
+  margin: 0 8px 12px;
   border: solid 1px #ccc;
   padding: 8px 12px;
   border-radius: 3px;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'mint-ui/lib/style.css'
+
 new Vue({
   el: '#app',
   render: h => h(App)
