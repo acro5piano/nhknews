f131d7ef60cc576fc406cda027740bc012f7796f
gosho-kazuya <ketsume0211@gmail.com>
2017-12-10 17:16:04 +0900

    add credit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
   <div id="app">
     <div class="header">
-      aaa
+      <span class="header-menu" @click="isMenuOpened = true">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+      </span>
+      NHK News
     </div>
 
+    <transition name="fade">
+      <div class="menu" v-if="isMenuOpened">
+        <div class="header">
+          <span class="header-menu" @click="isMenuOpened = false">
+            <i class="fa fa-times" aria-hidden="true"></i>
+          </span>
+        </div>
+        <div class="menu-content">
+          <p class="menu-content-title">Fast news</p>
+          <p class="menu-content-version">v0.0.1</p>
+          <p class="menu-content-credit">Kazuya Gosho</p>
+        </div>
+      </div>
+    </transition>
+
     <div class="spinner" v-if="loading">
-      <mt-spinner color="#33bd2a" type="snake"></mt-spinner>
+      <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
     </div>
 
     <div class="article-container" v-else>
@@ -24,17 +42,14 @@
 <script>
 import Vue from 'vue'
 import axios from 'axios'
-import { Spinner } from 'mint-ui';
-
-Vue.component(Spinner.name, Spinner);
 
 export default {
   name: 'app',
   data () {
     return {
       articles: [],
-      loading: true
-
+      loading: true,
+      isMenuOpened: false,
     }
   },
   mounted () {
@@ -42,6 +57,8 @@ export default {
       this.articles = res.data
       this.loading = false
     })
+  },
+  methods: {
   }
 }
 </script>
@@ -53,11 +70,12 @@ body {
   -moz-osx-font-smoothing: grayscale;
   color: #666;
   font-size: 12px;
+  margin-top: 48px;
+  background: #f7f7f7;
 }
 
 #app {
   line-height: 1.5;
-  background: #f7f7f7;
   box-sizing: border-box;
 }
 
@@ -68,15 +86,43 @@ h1, h2 {
 .header {
   height: 48px;
   background: #33bd2a;
+  border-bottom: solid 1px #33bd2a;
   position: fixed;
   top: 0;
   width: 100%;
   color: #fff;
-  font-size: 18px;
+  font-size: 16px;
   display: flex;
+  justify-content: flex-start;
   align-items: center;
-  justify-content: center;
-  box-shadow: 0 1px 4px rgba(0,0,0,.15);
+  box-shadow: 0 1px 4px rgba(0,0,0,.25);
+  padding: 0 12px;
+}
+.header-menu {
+  margin-right: 16px;
+  font-size: 22px;
+  padding: 6px;
+}
+
+.menu {
+  position: fixed;
+  height: 100vh;
+  width: 100%;
+  background: #fff;
+}
+.menu-content {
+  padding-top: 30vh;
+  text-align: center;
+  height: 80vh;
+}
+.menu-content-title {
+  font-size: 20px;
+  color: #333;
+}
+.menu-content-credit {
+  padding-top: 24px;
+  font-size: 12px;
+  color: #888;
 }
 
 .spinner {
@@ -87,9 +133,8 @@ h1, h2 {
 }
 
 .article-container {
-  padding-top: 60px;
+  padding-top: 12px;
 }
-
 .article {
   margin: 0 8px 12px;
   border: solid 1px #ccc;
@@ -97,7 +142,6 @@ h1, h2 {
   word-wrap: break-word;
   background: #fff;
 }
-
 .article-title {
   font-size: 16px;
   margin-bottom: 12px;
@@ -106,5 +150,12 @@ h1, h2 {
 }
 .article-content {
   padding: 4px 12px;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .3s
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import 'mint-ui/lib/style.css'
-
 new Vue({
   el: '#app',
   render: h => h(App)
