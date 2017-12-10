c7ddd18c0187b15a9e4d2d02ee7bd4ac4ee4516b
gosho-kazuya <ketsume0211@gmail.com>
2017-12-10 16:19:25 +0900

    build profile

--- provision/deploy.sh
+++ provision/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+yarn build
 aws s3 sync --delete dist/ s3://nhknews/ --profile kazuya
 aws configure set preview.cloudfront true
-aws cloudfront create-invalidation --distribution-id E3O29DRB7JJ6T --paths '/*'
+aws cloudfront create-invalidation --distribution-id E3O29DRB7JJ6T --paths '/*' --profile kazuya
