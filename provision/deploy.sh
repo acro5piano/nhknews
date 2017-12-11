32916030e6377c3d02e37ebd5efbebeb0d3ad116
gosho-kazuya <ketsume0211@gmail.com>
2017-12-12 00:38:47 +0900

    invoke lambda in deploy

--- provision/deploy.sh
+++ provision/deploy.sh
@@ -4,5 +4,6 @@ export AWS_PROFILE=kazuya
 
 yarn build
 aws s3 sync --delete dist/ s3://nhknews/
+aws lambda invoke --function-name nhknews /dev/stdin
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation --distribution-id E3O29DRB7JJ6T --paths '/*'
