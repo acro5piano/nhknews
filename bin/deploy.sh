3020f1e3e058a0f9fa1719454134a371c634b907
gosho-kazuya <ketsume0211@gmail.com>
2020-12-28 01:34:52 +0900

    change workflow name

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+export AWS_REGION=us-east-1
+
 yarn build
 aws s3 sync --delete dist/ s3://nhknews/
-aws lambda invoke --function-name nhknews /dev/stdin
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation --distribution-id E3O29DRB7JJ6T --paths '/*'
