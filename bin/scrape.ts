60424b313d019f125490941358d2bb459395cbd5
gosho-kazuya <ketsume0211@gmail.com>
2020-12-28 01:27:22 +0900

    change region

--- bin/scrape.ts
+++ bin/scrape.ts
@@ -11,6 +11,7 @@ import path from 'path'
 const cloudfront = new AWS.CloudFront()
 
 const s3 = new AWS.S3({
+  region: 'us-east-1',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
